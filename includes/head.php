881a0dbf0ee4e23f0c89aa7a4abc542b626b795c
David Delmar <ddelmar@where.com>
2013-12-31 14:03:10 -0500

    adding facebook stuff

---
+++ includes/head.php
@@ -0,0 +1,22 @@
+<!doctype>
+<html>
+
+<head>
+<meta charset="utf-8">
+
+<meta http-equiv="cache-control" content="no-cache" /> 
+<meta http-equiv="Content-Type" content="application/vnd.wap.xhtml+xml" /> 
+<meta name="HandheldFriendly" content="True" /> 
+<meta name="viewport" content="initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+<meta name="description" content="" />
+<link href="/c/master.css" rel="stylesheet" type="text/css" />
+
+<script type="text/javascript" src="/j/jquery-latest.min.js"></script>
+
+
+</head>
+
+<body>
+
+header content
+
